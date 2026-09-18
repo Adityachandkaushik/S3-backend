79513b28b23f3685a7ff4ef0bef0020cee16d7be
Aditya kaushik <165358967+Adityachandkaushik@users.noreply.github.com>
2026-09-18 11:23:40 +0530

    Fix formatting in galleryUpload.js

--- middleware/galleryUpload.js
+++ middleware/galleryUpload.js
@@ -4,7 +4,7 @@ import path from "path";
 const storage = multer.diskStorage({
   destination:"uploads/gallery",
   filename:(req,file,cb)=>{
-    cb(null, Date.now()+"-"+file.originalname);
+    cb(null, Date.now()+"-"+file.originalname);  
   }
 });
 
